5c4e8886ee34dc826f815f616f2244c2fc5a0163
Blake A. Wilson <blakeaw1102@gmail.com>
2020-10-15 15:24:03 -0500

    Fixed some bugs with the compartments and got the model running. I ended up replacing some macros with explicit rules and the Ca loc state variable doesn't change anymore since the location is denoted by the compartment modifier -- I may try to remove the loc state later.

--- parm/parm.py
+++ parm/parm.py
@@ -39,8 +39,8 @@ Revelant equations:
 
 '''
 
-from pysb import Model, Monomer, Parameter, Initial, Rule, Observable, Expression, Annotation, Compartment
-from pysb.macros import bind, bind_complex, catalyze, catalyze_complex
+from pysb import Model, Monomer, Parameter, Initial, Rule, Observable, Expression, Annotation, Compartment, ANY
+from pysb.macros import bind, bind_complex, catalyze, catalyze_complex, catalyze_state
 import numpy as np
 
 Model()
@@ -53,7 +53,7 @@ Vcell = 10.
 # but for simplicity we will assume it is 10% of cell volume.
 Ver = Vcell * 0.1
 # Default forward, reverse, and catalytic rates:
-KF = 1e-6 #
+KF = 1e-1 #
 KR = 1e-3 # Default dissociation rate from Albeck et al. https://doi.org/10.1371/journal.pbio.0060299
 KCAT = 10 # "average enzyme" from Bar-Even et al. https://doi.org/10.1021/bi2002289
 
@@ -61,17 +61,18 @@ KCAT = 10 # "average enzyme" from Bar-Even et al. https://doi.org/10.1021/bi2002
 # the 1 nM to 1 microM range assumed by Albeck et al. https://doi.org/10.1371/journal.pbio.0060299
 # for which they reference Wu and Pollard https://doi.org/10.1126/science.1113230
 SPC = 0.5
-
+#SPC = 10
 # As a first estimate for the forward binding reactions of Ca2+ we will assume
 # that it is diffusion-controlled following Smoluchowski eqn.:
 #     kf = 4*pi*D*R_o
 # We will assume R_o is 2 nm and that D = D_Ca2+.
 # The diffusion coefficient of Ca2+ is 5.3 x 10^-6 cm^2/s; https://doi.org/10.1016/0143-4160(87)90027-3
-D_Ca = 5.3e2 # micron^2/s
-R_o = 2e-3 # micron
+D_Ca = 5.3e-6 # cm^2/s
+R_o = 2e-7 # cm
 # Avogadro's number
 N_A = 6.022e23 # things/mol
-# the N_A*(1e-9) term is for unit conversion from micron^3/s to 1/s*microM
+# Multiplying by Avogadro's number converts molecular units into moles.
+# (1e-9) term is for unit conversion from cm^3/s*mol -> mL/s*mol -> 10^-3 L/s*mol -> 10^-3 1/s*M -> 10^-9 1/s*microM
 K_CA_BIND = 4*np.pi*D_Ca*R_o*N_A*(1e-9)  # 1/s*microM
 
 # Compartments
@@ -81,15 +82,15 @@ K_CA_BIND = 4*np.pi*D_Ca*R_o*N_A*(1e-9)  # 1/s*microM
 # Compartment('CONTAINER', dimension=2, size=SA_CM)
 # Assume extracellular volume is spherical with radius twice that of the
 # cell volume (hence the factor of 8 time the cell volume)
-Parameter('V_EXTRA', 8*Vcell)
+Parameter('V_EXTRA', 2*Vcell)
 Compartment('EXTRACELLULAR', dimension=3, size=V_EXTRA)
 Parameter('SA_CM', 4*np.pi*(3*Vcell/(4*np.pi))**1.5)
-Compartment('CELL_MEMB', dimension=2, size=SA_CM)
+Compartment('CELL_MEMB', dimension=2, size=SA_CM, parent=EXTRACELLULAR)
 Parameter('V_C', Vcell)
-Compartment('CYTOSOL', dimension=3, size=V_C)
+Compartment('CYTOSOL', dimension=3, size=V_C, parent=CELL_MEMB)
 #  ER membrane
 Parameter('SA_ER', 4*np.pi*(3*Ver/(4*np.pi))**1.5)
-Compartment('ER_MEMB', dimension=2, size=SA_ER, parent=CELL_MEMB)
+Compartment('ER_MEMB', dimension=2, size=SA_ER, parent=CYTOSOL)
 #  internal ER lumen space
 Parameter('V_ER', Ver)
 Compartment('ER_LUMEN', dimension=3, size=V_ER, parent=ER_MEMB)
@@ -128,11 +129,11 @@ Annotation(IP3R, 'https://identifiers.org/uniprot:Q14643')
 
 # Initial conditions
 # ==================
-# PAR2 agonist 2AT, Kang et al. https://doi.org/10.1021/acsnano.9b01993
+# PAR2 agonist 2AT, 330 nM from Kang et al. https://doi.org/10.1021/acsnano.9b01993
 Parameter('TAT_0', 330e-3)
 Initial(TAT(b=None)**EXTRACELLULAR, TAT_0)
 # inactive PAR2
-Parameter('PAR2_0', SPC*V_C.value/SA_CM.value)
+Parameter('PAR2_0', SPC*V_C.value/(SA_CM.value*100)) # Had to convert to area concentration.
 Initial(PAR2(state='I', b=None)**CELL_MEMB, PAR2_0)
 # inactive Gaq
 Parameter('Gaq_0', SPC)
@@ -144,7 +145,7 @@ Initial(PLC(bgaq=None, bpip2=None)**CYTOSOL, PLC_0)
 Parameter('PIP2_0', SPC)
 Initial(PIP2(b=None)**CYTOSOL, PIP2_0)
 # IP3R
-Parameter('IP3R_0', SPC*V_ER.value/SA_ER.value)
+Parameter('IP3R_0', SPC*V_ER.value/SA_ER.value) # Had to convert to area concentration.
 Initial(IP3R(b1=None, b2=None, b3=None, b4=None, bcaer=None, bcacyt=None)**ER_MEMB, IP3R_0)
 # ER Ca2+ store
 # ER lumen of HEK-293 cells has between roughly 400-600 microM with an average
@@ -152,6 +153,8 @@ Initial(IP3R(b1=None, b2=None, b3=None, b4=None, bcaer=None, bcacyt=None)**ER_ME
 # Foyouzi-Youssefi et al. https://doi.org/10.1073/pnas.97.11.5723 (Fig. 3C, control)
 Parameter('Ca_0', 525)
 Initial(Ca(loc='E', b=None)**ER_LUMEN, Ca_0)
+#Parameter('Ca_C_0', 10)
+#Initial(Ca(loc='C', b=None)**CYTOSOL, Ca_C_0)
 # TN-XXL
 Parameter('TNXXL_0', 0.08)
 Initial(TNXXL(bca=None)**CYTOSOL, TNXXL_0)
@@ -176,17 +179,21 @@ Parameter('kf_PLC_bind_PIP2', KF)
 Parameter('kr_PLC_bind_PIP2', KR)
 Parameter('kcat_PIP2_to_IP3', KCAT)
 # Binding of IP3 to IP3R
-Parameter('kf_IP3_bind_IP3R', KF)
-Parameter('kr_IP3_bind_IP3R', KR)
+Parameter('kf_IP3_bind_IP3R', KF*100)
+Parameter('kr_IP3_bind_IP3R', KR/10)
 # Transport of Ca2+
 #  ER -> cytosol:
 Parameter('kf_erCa_bind_IP3R', K_CA_BIND)
 Parameter('kr_erCa_bind_IP3R', KR)
-Parameter('kcat_tranport_erCa', KCAT)
+#Parameter('kf_erCa_bind_IP3R', KF)
+#Parameter('kr_erCa_bind_IP3R', KR)
+Parameter('kcat_tranport_erCa', KCAT/10)
 #  cytosol -> ER:
 Parameter('kf_cytCa_bind_IP3R', K_CA_BIND)
 Parameter('kr_cytCa_bind_IP3R', KR)
-Parameter('kcat_tranport_cytCa', KCAT/10.)
+#Parameter('kf_cytCa_bind_IP3R', KF/10)
+#Parameter('kr_cytCa_bind_IP3R', KR*10)
+Parameter('kcat_tranport_cytCa', KCAT/100.)
 # Ca2+ binding to TN-XXL FRET reporter
 #  Kd = 800 nM, https://doi.org/10.1038/nmeth.1243
 Parameter('Kd_cytCa_bind_TNXXL', 800e-3)
@@ -198,52 +205,107 @@ Expression('kr_cytCa_bind_TNXXL', kf_cytCa_bind_TNXXL*Kd_cytCa_bind_TNXXL)
 # =====
 # PAR2 activation by 2AT:
 #    2AT + PAR2_I <---> TAT:PAR2_I ---> TAT + PAR2_A
-catalyze(TAT()**EXTRACELLULAR, 'b', PAR2(state='I')**CELL_MEMB, 'b', PAR2(state='A')**CELL_MEMB,
-         (kf_PAR2_bind_TAT,kr_PAR2_bind_TAT, kcat_activate_PAR2))
+catalyze_state(TAT()**EXTRACELLULAR, 'b', PAR2()**CELL_MEMB, 'b', 'state', 'I', 'A',
+         [kf_PAR2_bind_TAT,kr_PAR2_bind_TAT, kcat_activate_PAR2])
 # Gaq activation by activated-PAR2:
 #    PAR2_A + Gaq_I <---> PAR2_A:Gaq_I ---> PAR2_A + Gaq_A
-catalyze(PAR2(state='A')**CELL_MEMB, 'b', Gaq(state='I')**CYTOSOL, 'b', Gaq(state='A')**CYTOSOL,
-         (kf_PAR2_bind_Gaq,kr_PAR2_bind_Gaq,kcat_activate_Gaq))
+catalyze_state(PAR2(state='A')**CELL_MEMB, 'b', Gaq()**CYTOSOL, 'b', 'state', 'I', 'A',
+               [kf_PAR2_bind_Gaq,kr_PAR2_bind_Gaq,kcat_activate_Gaq])
 # PLC activation by binding Gaq:
 #    Gaq_A + PLC <---> Gaq_A:PLC
-bind(Gaq(state='A')**CYTOSOL, 'b', PLC()**CYTOSOL, 'bgaq', (kf_PLC_bind_Gaq,kr_PLC_bind_Gaq))
+bind(Gaq(state='A')**CYTOSOL, 'b', PLC()**CYTOSOL, 'bgaq', [kf_PLC_bind_Gaq,kr_PLC_bind_Gaq])
 # Conversion of PIP2 to IP3
 #    Gaq_A:PLC + PIP2 <---> Gaq_A:PLC:PIP2 ---> Gaq_A:PLC + IP3
 catalyze_complex(PLC(bgaq=1)**CYTOSOL % Gaq(b=1, state='A')**CYTOSOL, 'bpip2', PIP2()**CYTOSOL, 'b', IP3(b=None)**CYTOSOL,
-                (kf_PLC_bind_PIP2,kr_PLC_bind_PIP2,kcat_PIP2_to_IP3))
+                 [kf_PLC_bind_PIP2,kr_PLC_bind_PIP2,kcat_PIP2_to_IP3])
 # Binding of IP3 to IP3R - IP3R is activated when all 4 subunits are bound
 #   IP3R + IP3 <---> IP3R:IP3, subunit 1
-bind(IP3R()**ER_MEMB, 'b1', IP3()**CYTOSOL, 'b', (kf_IP3_bind_IP3R,kr_IP3_bind_IP3R))
+bind(IP3R(b2=None,b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB, 'b1', IP3(b=None)**CYTOSOL, 'b', [kf_IP3_bind_IP3R,kr_IP3_bind_IP3R])
 #   IP3R + IP3 <---> IP3R:IP3, subunit 2
-bind_complex(IP3R(b1=1)**ER_MEMB % IP3(b=1)**CYTOSOL, 'b2', IP3()**CYTOSOL, 'b',
-             (kf_IP3_bind_IP3R,kr_IP3_bind_IP3R))
+#bind_complex(IP3R(b1=1,b2=None,b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB % IP3(b=1)**CYTOSOL, 'b2', IP3(b=None)**CYTOSOL, 'b',
+#             [kf_IP3_bind_IP3R,kr_IP3_bind_IP3R])
+Rule('bind_IP3_IPR3_sub2', IP3R(b1=1,b2=None,b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB
+     % IP3(b=1)**CYTOSOL + IP3(b=None)**CYTOSOL |
+     IP3R(b1=1,b2=2,b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL, kf_IP3_bind_IP3R, kr_IP3_bind_IP3R)
 #   IP3R + IP3 <---> IP3R:IP3, subunit 3
-bind_complex(IP3R(b1=1, b2=2)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL, 'b3', IP3()**CYTOSOL, 'b',
-             (kf_IP3_bind_IP3R,kr_IP3_bind_IP3R))
+#bind_complex(IP3R(b1=1, b2=50, b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=50)**CYTOSOL, 'b3', IP3(b=None)**CYTOSOL, 'b',
+#            [kf_IP3_bind_IP3R,kr_IP3_bind_IP3R])
+Rule('bind_IP3_IPR3_sub3',
+     IP3R(b1=1,b2=2,b3=None,b4=None,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL + IP3(b=None)**CYTOSOL |
+     IP3R(b1=1,b2=2,b3=3,b4=None,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL,
+     kf_IP3_bind_IP3R, kr_IP3_bind_IP3R)
 #   IP3R + IP3 <---> IP3R:IP3, subunit 4
-bind_complex(IP3R(b1=1, b2=2,b3=3)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL, 'b4',
-             IP3()**CYTOSOL, 'b', (kf_IP3_bind_IP3R,kr_IP3_bind_IP3R))
+#bind_complex(IP3R(b1=1, b2=50,b3=50,b4=None)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=50)**CYTOSOL % IP3(b=50)**CYTOSOL, 'b4',
+#             IP3(b=None)**CYTOSOL, 'b', [kf_IP3_bind_IP3R,kr_IP3_bind_IP3R])
+Rule('bind_IP3_IPR3_sub4',
+     IP3R(b1=1,b2=2,b3=3,b4=None,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL +
+     IP3(b=None)**CYTOSOL |
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL
+     % IP3(b=4)**CYTOSOL,
+     kf_IP3_bind_IP3R, kr_IP3_bind_IP3R)
 # Transport of Ca2+ by activated IP3R
 #  ER -> cytosol:
 #    IP3R:IP3_4 + Ca_E <---> Ca_E:IP3R:IP3_4 ---> Ca_C + IP3R:IP3_4
-catalyze_complex(IP3R(b1=1, b2=2, b3=3, b4=4)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL
-                 % IP3(b=3)**CYTOSOL % IP3(b=4)**CYTOSOL, 'bcaer', Ca(loc='E')**ER_LUMEN, 'b', Ca(loc='C', b=None)**CYTOSOL,
-                 (kf_erCa_bind_IP3R, kr_erCa_bind_IP3R, kcat_tranport_erCa))
+#catalyze_complex(IP3R(b1=1, b2=2, b3=3, b4=4,bcaer=None,bcacyt=None)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL
+#                  % IP3(b=3)**CYTOSOL % IP3(b=4)**CYTOSOL, 'bcaer', Ca(loc='E', b=None)**ER_LUMEN, 'b', Ca(loc='C', b=None)**CYTOSOL,
+#                  [kf_erCa_bind_IP3R, kr_erCa_bind_IP3R, kcat_tranport_erCa])
+Rule('bind_Ca_IPR3_er',
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL %
+     IP3(b=4)**CYTOSOL + Ca(loc='E', b=None)**ER_LUMEN |
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=5,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL
+     % IP3(b=4)**CYTOSOL % Ca(loc='E',b=5)**ER_LUMEN,
+     kf_erCa_bind_IP3R, kr_erCa_bind_IP3R)
+Rule('transport_Ca_ER_CYTO',
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=5,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL
+     % IP3(b=4)**CYTOSOL % Ca(loc='E',b=5)**ER_LUMEN >>
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL %
+     IP3(b=4)**CYTOSOL + Ca(loc='E', b=None)**CYTOSOL, kcat_tranport_erCa)
+#Rule('transport_Ca_ER_CYTO', Ca(loc='E', b=None)**ER_LUMEN >> Ca(loc='E', b=None)**CYTOSOL, kcat_tranport_erCa)
+
 #  Reverse, cytosol -> ER:
 #    IP3R:IP3_4 + Ca_C <---> Ca_C:IP3R:IP3_4 ---> Ca_E + IP3R:IP3_4
-catalyze_complex(IP3R(b1=1, b2=2, b3=3, b4=4)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL
-                 % IP3(b=3)**CYTOSOL % IP3(b=4)**CYTOSOL, 'bcacyt', Ca(loc='C')**CYTOSOL, 'b', Ca(loc='E', b=None)**ER_LUMEN,
-                 (kf_cytCa_bind_IP3R, kr_cytCa_bind_IP3R, kcat_tranport_cytCa))
+#catalyze_complex(IP3R(b1=1, b2=2, b3=3, b4=4,bcaer=None,bcacyt=None)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL
+#                  % IP3(b=3)**CYTOSOL % IP3(b=4)**CYTOSOL, 'bcacyt', Ca(loc='C', b=None)**CYTOSOL, 'b', Ca(loc='E', b=None)**ER_LUMEN,
+#                  [kf_cytCa_bind_IP3R, kr_cytCa_bind_IP3R, kcat_tranport_cytCa])
+Rule('bind_Ca_IPR3_cyto',
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL %
+     IP3(b=4)**CYTOSOL + Ca(loc='E', b=None)**CYTOSOL |
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=5)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL
+     % IP3(b=4)**CYTOSOL % Ca(loc='E',b=5)**CYTOSOL,
+     kf_erCa_bind_IP3R, kr_erCa_bind_IP3R)
+Rule('transport_Ca_CYTO_ER',
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=5)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL
+     % IP3(b=4)**CYTOSOL % Ca(loc='E',b=5)**CYTOSOL >>
+     IP3R(b1=1,b2=2,b3=3,b4=4,bcaer=None,bcacyt=None)**ER_MEMB %
+     IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL %
+     IP3(b=4)**CYTOSOL + Ca(loc='E', b=None)**ER_LUMEN, kcat_tranport_cytCa)
+
 
 # Binding of Calcium to the TN-XXL FRET reporter
-bind(TNXXL()**CYTOSOL, 'bca', Ca(loc='C')**CYTOSOL, 'b',
+bind(TNXXL(bca=None)**CYTOSOL, 'bca', Ca(loc='E',b=None)**CYTOSOL, 'b',
      (kf_cytCa_bind_TNXXL,kr_cytCa_bind_TNXXL))
 
 # Observables
 # ===========
 Observable('iPAR2', PAR2(state='I'))
 Observable('aPAR2', PAR2(state='A'))
-Observable('cytoCa', Ca(loc='C', b=None))
+Observable('aIP3R', IP3R(b1=1, b2=2, b3=3, b4=4)**ER_MEMB % IP3(b=1)**CYTOSOL % IP3(b=2)**CYTOSOL % IP3(b=3)**CYTOSOL % IP3(b=4)**CYTOSOL)
+Observable('iIP3R', IP3R(b1=None,b2=None,b3=None,b4=None))
+Observable('erCa', Ca(loc='E', b=None)**ER_LUMEN)
+Observable('cytoCa', Ca(loc='E', b=None)**CYTOSOL)
 # Get the FRET signal
-Observable('fret_complex', TNXXL(bca=1)%Ca(b=1,loc='C'))
-Expression('fret_signal', fret_complex/TNXXL_0)
+Observable('fret_complex', TNXXL(bca=1)**CYTOSOL % Ca(b=1,loc='E')**CYTOSOL)
+Expression('FRET', fret_complex/TNXXL_0)
+#Observable('FRET', fret_signal)
