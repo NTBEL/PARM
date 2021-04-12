a2e93eadf4c57dace4978fa313b10bbd569ffe5b
Blake A. Wilson <blakeaw1102@gmail.com>
2021-04-12 16:05:40 -0500

    Updated the init to include new model variants.

--- parm/__init__.py
+++ parm/__init__.py
@@ -1,3 +1,5 @@
-from .classic import model as classic
-from .classic import nM_to_num_per_pL, microM_to_num_per_pL, nM_2AT_to_num
-from .precoupled import model as precoupled
+from .classic_1 import model as classic_1
+from .classic_1 import nM_to_num_per_pL, microM_to_num_per_pL, nM_2AT_to_num
+from .classic_2 import model as classic_2
+from .precoupled_1 import model as precoupled_1
+from .precoupled_2 import model as precoupled_2
