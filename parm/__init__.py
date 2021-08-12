5a5bfb8f0cd6c1d16697de24dd9ad884d35bb0fb
Blake A. Wilson <blakeaw1102@gmail.com>
2021-08-12 14:08:23 -0500

    Added catalytic model imports to init.

--- parm/__init__.py
+++ parm/__init__.py
@@ -5,5 +5,8 @@ from .classic_2f import model as classic_2f
 from .precoupled_1 import model as precoupled_1
 from .precoupled_2m import model as precoupled_2m
 from .precoupled_2f import model as precoupled_2f
+from .catalytic_1 import model as catalytic_1
+from .catalytic_23 import model as catalytic_23
+
 # Import conversion factors
 from .classic_1 import nM_to_num_per_pL, microM_to_num_per_pL, nM_2AT_to_num
