745ee5fa35e77dce0742436ea3544206dd95cd9c
blakeaw <blakeaw1102@gmail.com>
2022-04-05 10:52:25 -0500

    Added output of the equilibrated value of the ER lumen Ca2+ amount for the pre_equilibration function.

--- parm/util.py
+++ parm/util.py
@@ -2,11 +2,13 @@ import numpy as np
 from pysb.simulator import ScipyOdeSimulator
 from itertools import compress
 
+
 def run_model(model, tspan, param_values):
-    solver = ScipyOdeSimulator(model, tspan=tspan, integrator='lsoda')
+    solver = ScipyOdeSimulator(model, tspan=tspan, integrator="lsoda")
     m_run = solver.run()
     yout = m_run.all
     return yuot
+
 
 # Adapted from: https://github.com/LoLab-VU/JARM/blob/master/model_analysis/equilibration_function.py
 def pre_equilibration(model, time_search, parameters=None, tolerance=1e-6):
@@ -28,7 +30,9 @@ def pre_equilibration(model, time_search, parameters=None, tolerance=1e-6):
     -------
     """
     # Solve system for the time span provided
-    solver = ScipyOdeSimulator(model, tspan=time_search, param_values=parameters, integrator='lsoda').run()
+    solver = ScipyOdeSimulator(
+        model, tspan=time_search, param_values=parameters, integrator="lsoda"
+    ).run()
     if solver.nsims == 1:
         simulations = [solver.species]
     else:
@@ -43,30 +47,44 @@ def pre_equilibration(model, time_search, parameters=None, tolerance=1e-6):
         for idx in range(y.shape[1]):
             sp_eq = False
             derivative = np.diff(y[:, idx]) / dt
-            derivative_range = ((derivative < tolerance) & (derivative > -tolerance))
+            derivative_range = (derivative < tolerance) & (derivative > -tolerance)
             # Indexes of values less than tolerance and greater than -tolerance
-            derivative_range_idxs = list(compress(range(len(derivative_range)), derivative_range))
+            derivative_range_idxs = list(
+                compress(range(len(derivative_range)), derivative_range)
+            )
             for i in derivative_range_idxs:
                 # Check if derivative is close to zero in the time points ahead
                 if i + 3 > len(time_search):
-                    raise Exception('Equilibrium can not be reached within the time_search input')
+                    raise Exception(
+                        "Equilibrium can not be reached within the time_search input"
+                    )
                 if (derivative[i + 3] < tolerance) | (derivative[i + 3] > -tolerance):
                     sp_eq = True
                     if time_search[i] > time_to_equilibration[0]:
                         time_to_equilibration[0] = time_search[i]
                         time_to_equilibration[1] = i
                 if not sp_eq:
-                    raise Exception('Equilibrium can not be reached within the time_search input')
+                    raise Exception(
+                        "Equilibrium can not be reached within the time_search input"
+                    )
                 if sp_eq:
                     break
             else:
-                raise Exception('Species s{0} ({1}) has not reached equilibrium'.format(idx,model.species[idx]))
+                raise Exception(
+                    "Species s{0} ({1}) has not reached equilibrium".format(
+                        idx, model.species[idx]
+                    )
+                )
 
         conc_eq = y[time_to_equilibration[1]]
         all_times_eq[n] = time_to_equilibration
         all_conc_eq[n] = conc_eq
     if solver.nsims == 1:
-        cytoCa_eq = [solver.observables['cytoCa'][time_to_equilibration[1]]]
+        cytoCa_eq = [solver.observables["cytoCa"][time_to_equilibration[1]]]
+        erCa_eq = [solver.observables["erCa"][time_to_equilibration[1]]]
     else:
-        cytoCa_eq = [obs['cytoCa'][time_to_equilibration[1]] for obs in solver.observables]
+        cytoCa_eq = [
+            obs["cytoCa"][time_to_equilibration[1]] for obs in solver.observables
+        ]
+        erCa_eq = [obs["erCa"][time_to_equilibration[1]] for obs in solver.observables]
     return all_times_eq, all_conc_eq, cytoCa_eq
