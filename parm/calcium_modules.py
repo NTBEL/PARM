e814c7512b270280d47173c138d2e9b72bb447de
blakeaw <blakeaw1102@gmail.com>
2022-04-11 14:12:21 -0500

    Added a Hill Coefficient to the parameters for the the extrusion rate.

--- parm/calcium_modules.py
+++ parm/calcium_modules.py
@@ -593,9 +593,10 @@ def calcium_extrusion_and_influx_mk():
     alias_model_components()
     # cytosol to extracellular space
     # Set a nominal max rate of 20 uM/s.
-    Parameter("Vmax_Ca_cyt_to_extra", 10 * units.microM_to_molec_per_pL * Vcyto.value)
+    Parameter("Vmax_Ca_cyt_to_extra", 10000 * units.nM_to_molec_per_pL * Vcyto.value)
     # Nominal value of 1 uM.
-    Parameter("Km_Ca_cyt_to_extra", 1 * units.microM_to_molec_per_pL * Vcyto.value)
+    Parameter("Km_Ca_cyt_to_extra", 0.15 * units.microM_to_molec_per_pL * Vcyto.value)
+    Parameter("n_Ca_cyt_to_extra", 2)
     # extracellular space to cytosol
     # Nominal value of 10 mM.
     Parameter("Km_Ca_extra_to_cyt", 10000 * units.microM_to_molec_per_pL * Ver.value)
@@ -604,9 +605,15 @@ def calcium_extrusion_and_influx_mk():
     Parameter(
         "Vmax_Ca_extra_to_cyt",
         Vmax_Ca_cyt_to_extra.value
-        * Ca_C_0.value
+        * Ca_C_0.value ** n_Ca_cyt_to_extra.value
         * (Km_Ca_extra_to_cyt.value + Ca_extra_0.value)
-        / ((Km_Ca_cyt_to_extra.value + Ca_C_0.value) * Ca_extra_0.value),
+        / (
+            (
+                Km_Ca_cyt_to_extra.value ** n_Ca_cyt_to_extra.value
+                + Ca_C_0.value ** n_Ca_cyt_to_extra.value
+            )
+            * Ca_extra_0.value
+        ),
     )  # 10 * units.microM_to_molec_per_pL * Vcyto.value)
     # Some 'private' observables to monitor the ER and cytoslic calcium for
     # Michaelis-Menten rate.
@@ -615,7 +622,9 @@ def calcium_extrusion_and_influx_mk():
     alias_model_components()
     # First order rate constants based on MK rate.
     Expression(
-        "k_Ca_cyt_to_extra", Vmax_Ca_cyt_to_extra / (_Ca_cyt + Km_Ca_cyt_to_extra)
+        "k_Ca_cyt_to_extra",
+        (Vmax_Ca_cyt_to_extra * _Ca_cyt ** (n_Ca_cyt_to_extra - 1))
+        / (_Ca_cyt ** n_Ca_cyt_to_extra + Km_Ca_cyt_to_extra ** n_Ca_cyt_to_extra),
     )  # 1/s
     Expression(
         "k_Ca_extra_to_cyt", Vmax_Ca_extra_to_cyt / (_Ca_extra + Km_Ca_extra_to_cyt)
