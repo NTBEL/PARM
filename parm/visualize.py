02bc78ccde9b1bef35ff9ecca3bd194e75bd2d48
blakeaw <blakeaw1102@gmail.com>
2023-02-13 13:52:19 -0600

    New function to display a comparison of the simulated and experimental 2AT dose-response.

--- parm/visualize.py
+++ parm/visualize.py
@@ -3,9 +3,12 @@ import typing
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pysb
 
 from . import data
+from . import util
+from . import units
 
 EXP_2AT_CONCS = {"10":10.,
                   "31.6":31.6,
@@ -60,7 +63,7 @@ def kang2019_fig_s3c():
 
 def kang2019_fig_s3d():
     figs3d = data.kang2019_fig_s3d()
-    plt.errorbar(figs3d['[2AT]'], figs3d['Max.dRR'], figs3d['Max.dRR_err'], marker='s', linestyle="", color='black', capsize=2.5)
+    plt.errorbar(figs3d['[2AT]'], figs3d['Max.dRR'], figs3d['Max.dRR_err'], marker='s', linestyle="", color='black', capsize=2.5, label='Exp.')
     plt.xscale('log')
     plt.ylabel(r'Max. $\Delta$R/R')
     plt.xlabel('[2AT] (nM)')
@@ -83,7 +86,7 @@ def display_expcomp_single(
     # Experimental data is in PARM/exp_data
     exp_data = data.training_data()
     times = exp_data["Time"]
-    tspan, fidx = expand_times(times)
+    tspan, fidx = util.expand_times(times)
     kang2019_fig_s3c()
 
     positions = list()
@@ -92,14 +95,14 @@ def display_expcomp_single(
         positions.append(param_values.copy())
     sim_out = util.run_model(model, tspan, param_values=positions, nprocs=nprocs)
     for i, key in enumerate(EXP_2AT_CONCS.keys()):
-        ysim = sim[i]["FRET"]
+        ysim = sim_out[i]["FRET"]
         plt.plot(tspan, ysim, label=None, color=EXP_PLT_COLORS[key])
     if save:
         plt.savefig("display_expcomp_single.png")
 
 def display_expcomp_multi_grid_mean_ci(
     model: pysb.Model,
-    positions: : typing.Union[np.ndarray, typing.List[np.ndarray]],
+    positions: typing.Union[np.ndarray, typing.List[np.ndarray]],
     idxs_mask: list,
     counts: typing.Union[None, np.ndarray] = None,
     nprocs: int = 1,
@@ -114,10 +117,10 @@ def display_expcomp_multi_grid_mean_ci(
     # Experimental data is in PARM/exp_data
     exp_data = data.training_data()
     times = exp_data["Time"]
-    tspan, fidx = expand_times(times)
+    tspan, fidx = util.expand_times(times)
     if counts is None:
         counts = np.array([1]*len(positions))
-    f, axes = plt.subplots(2, 3, figsize=(4, 7), sharex=True, sharey=True)
+    f, axes = plt.subplots(2, 3, figsize=(7, 4), sharex=True, sharey=True)
     new_positions = list()
     for position in positions:
         Y = 10 ** position
@@ -129,7 +132,7 @@ def display_expcomp_multi_grid_mean_ci(
             position[twoat_mask] = tat_number
         sim_out = util.run_model(model, tspan, param_values=new_positions, nprocs=nprocs)
         fret_sims = list()
-        for i, s in enumerate(sim_out):
+        for s in sim_out:
             fret_sims.append(s["FRET"])
         fret_sims = np.array(fret_sims)
         fret_mean = np.average(fret_sims, axis=0, weights=counts)
@@ -146,18 +149,124 @@ def display_expcomp_multi_grid_mean_ci(
         # 5: [1, 2]
         ax_row = int(i / 3)
         ax_col = int((i - 3*ax_row))
+        print(i, ax_row, ax_col)
         axes[ax_row, ax_col].errorbar(times, exp_data[col_sig], exp_data[col_err],
-                     capsize=2.5, label=key, color=EXP_PLT_COLORS[key],
-                     marker=EXP_PLT_MARKERS[key])
+                     capsize=2.5, label="Exp.", color=EXP_PLT_COLORS[key],
+                     marker=EXP_PLT_MARKERS[key], linestyle=' ')
 
         axes[ax_row, ax_col].plot(tspan, fret_mean, color=EXP_PLT_COLORS[key],
                         linewidth=2, label="Sim.")
         axes[ax_row, ax_col].fill_between(
             tspan,
-            fret_mean - fret_std,
-            fret_mean + fret_std,
+            fret_mean - 1.96 * fret_std,
+            fret_mean + 1.96 * fret_std,
             alpha=0.25,
             color=EXP_PLT_COLORS[key],
         )
+        axes[ax_row, ax_col].set_title("[2AT] = {} nM".format(key), fontdict={"fontsize": 10})
+        axes[ax_row, ax_col].legend(loc=0, frameon=False)           
+    for ax in axes.flat:
+        ax.set(xlabel="Time (s)", ylabel=r"$\Delta$R/R ")
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axes.flat:
+        ax.label_outer()
+    plt.tight_layout()    
     if save:
         plt.savefig("display_expcomp_multi_grid_mean_ci.png")
+
+def plot_marginal_dists(
+    model: pysb.Model,
+    samples: typing.Union[np.ndarray, typing.List[np.ndarray]],
+    idxs_mask: list,
+    nbins: int = 50,
+    nprocs: int = 1,
+    save: bool = False,
+    ):
+    """Plot the marginal distributions for the sampled parameters.
+    Adapted from:
+    https://github.com/LoLab-VU/JARM/blob/master/model_analysis/pars_dists_plot.py
+    """
+    
+    ndims = len(idxs_mask)
+    colors = sns.color_palette(n_colors=ndims)
+    columns = 4
+    rows = int(ndims / columns)
+    if (ndims % columns) > 0:
+        rows += 1
+    counter = 0
+
+    f, axes = plt.subplots(rows, columns, figsize=(7, 8), sharex=True)
+    for r in range(rows):
+        for c in range(columns):
+            weights = np.ones_like(samples[:, counter])/float(len(samples[:, counter]))
+            axes[r, c].hist(samples[:, counter], bins=nbins, color=colors[counter], weights=weights)
+            axes[r, c].set_title(model.parameters[idxs_mask[counter]].name, fontdict={'fontsize':8})
+            # axes[r, c].set_xlim(-6, 6)
+            counter += 1
+
+            if counter >= len(idxs_mask):
+                break
+    f.add_subplot(111, frameon=False)
+    f.subplots_adjust(wspace=0.4)
+    f.subplots_adjust(hspace=0.5)
+    # hide tick and tick label of the big axes
+    plt.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+    plt.grid(False)
+    plt.xlabel("Log10(Parameter value)", fontsize=14)
+    plt.ylabel("Probability", fontsize=14, labelpad=15)
+    plt.tight_layout()
+    # plt.show()
+    if save:
+        plt.savefig('pars_dist_plot_{}bins.png'.format(nbins), format='png', bbox_inches="tight")
+    
+    return
+
+def display_dose_response_comparison(
+    model: pysb.Model,
+    positions: typing.Union[np.ndarray, typing.List[np.ndarray]],
+    idxs_mask: list,
+    counts: typing.Union[None, np.ndarray] = None,
+    nprocs: int = 1,
+    save: bool = False,
+    ):
+    """Runs the model and plots a comparison with the experimental data for 2AT response."""
+
+
+    param_values = np.array([param.value for param in model.parameters])
+    twoat_mask = [param.name == "TAT_0" for param in model.parameters]
+
+    # Experimental data is in PARM/exp_data
+    exp_data = data.training_data()
+    times = exp_data["Time"]
+    tspan, fidx = util.expand_times(times)
+    if counts is None:
+        counts = np.array([1]*len(positions))
+    new_positions = list()
+    for position in positions:
+        Y = 10 ** position
+        param_values[idxs_mask] = Y
+        new_positions.append(param_values.copy())
+    #dr_exp = data.kang2019_fig_s3d()
+    kang2019_fig_s3d()
+    dr_sim = list()    
+    for i, key in enumerate(EXP_2AT_CONCS.keys()):
+        tat_number = EXP_2AT_CONCS[key] * units.nM_to_molec_per_pL * model.parameters["Vextra"].value
+        for position in new_positions:
+            position[twoat_mask] = tat_number
+        sim_out = util.run_model(model, tspan, param_values=new_positions, nprocs=nprocs)
+        fret_sims = list()
+        for s in sim_out:
+            fret_sims.append(s["FRET"])
+        fret_sims = np.array(fret_sims)
+        fret_mean = np.average(fret_sims, axis=0, weights=counts)
+        fret_std = np.sqrt(
+            np.average(np.subtract(fret_sims, fret_mean) ** 2, axis=0, weights=counts)
+        )
+        idx_max = np.argmax(fret_mean)
+        dr_sim.append([EXP_2AT_CONCS[key], fret_mean[idx_max], fret_std[idx_max]])
+    dr_sim = np.array(dr_sim)
+    plt.errorbar(dr_sim[:,0], dr_sim[:,1], yerr=dr_sim[:,2], color='r', label='Sim.', marker='o', linestyle=' ')
+    plt.legend(loc=0, frameon=False)
+    plt.tight_layout()    
+    if save:
+        plt.savefig("display_dose_response_comparison.png")
